d1d32fad384a2c4d6a8f2361e0e645394c95c2c2
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-12-08 11:23:19 +0100

    class definition with private attributes and public methods so-called constructor

--- firends.h
+++ firends.h
@@ -10,3 +10,13 @@ class Punkt
         Punkt(string="A",float=0, float=0);
         void wczytaj();
 };
+
+class Prostokat
+{
+    string nazwa;
+    float x,y,szerokosc,wysokosc;
+
+public:
+    Prostokat(string="brak",float=0,float=0,float=1,float=1);
+    void wczytaj();
+};

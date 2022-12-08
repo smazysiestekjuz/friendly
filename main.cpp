065fed45e48e8a7cdaa4720f776967997d9bd8d5
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-12-08 13:00:37 +0100

    simple test coordinates

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "friends.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Punkt pkt1("A",3,1);
+   // pkt1.wczytaj();
+
+    Prostokat p1("Prostokat",0,0,6,4);
+   // p1.wczytaj();
+
     return 0;
 }
